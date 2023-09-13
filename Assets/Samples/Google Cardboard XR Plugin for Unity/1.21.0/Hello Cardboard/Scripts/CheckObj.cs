e5e80c3876722dc2e778f8d362d9d2e7f25fe042
Yash Parkhi <83068397+yiungyiung@users.noreply.github.com>
2023-09-14 01:38:50 +0530

    helu

--- Assets/Samples/Google Cardboard XR Plugin for Unity/1.21.0/Hello Cardboard/Scripts/CheckObj.cs
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.21.0/Hello Cardboard/Scripts/CheckObj.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class CheckObj : MonoBehaviour
 {
-    enum button{
-        buy,
-        sell,
-        plus,
-        minus
+   public enum button{
+        Buy,
+        Sell,
+        Plus,
+        Minus,
+        Book,
+        Cheque,
+        Cut,
     }
-
+    public GameObject Book;
     [SerializeField]
     private Color inatctiveColor;
 
@@ -18,7 +21,7 @@ public class CheckObj : MonoBehaviour
     private Color gazedAtColor;
 
     private MeshRenderer myRenderer;
-
+    public button selectedButton;
     void Start()
     {
         myRenderer = GetComponent<MeshRenderer>();
@@ -42,9 +45,75 @@ public class CheckObj : MonoBehaviour
 
     public void OnPointerClick()
     {
-        Debug.LogError("clicked");
-        Destroy(gameObject);
+        switch (selectedButton)
+        {
+            case button.Buy:
+                BuyFunction();
+                break;
+            case button.Sell:
+                SellFunction();
+                break;
+            case button.Plus:
+                PlusFunction();
+                break;
+            case button.Minus:
+                MinusFunction();
+                break;
+            case button.Book:
+                BookFunction();
+                break;
+            case button.Cheque:
+                ChequeFunction();
+                break;
+            case button.Cut:
+                CutBookFunction();
+                break;
     }
+    }
+
+         void BuyFunction()
+    {
+        // Add code for the "Buy" button functionality here.
+        Debug.Log("Buy button clicked.");
+    }
+
+     void SellFunction()
+    {
+        // Add code for the "Sell" button functionality here.
+        Debug.Log("Sell button clicked.");
+    }
+
+     void PlusFunction()
+    {
+        // Add code for the "Plus" button functionality here.
+        Debug.Log("Plus button clicked.");
+    }
+
+     void MinusFunction()
+    {
+        // Add code for the "Minus" button functionality here.
+        Debug.Log("Minus button clicked.");
+    }
+
+     void BookFunction()
+    {
+        Book.SetActive(true);
+        gameObject.SetActive(false);
+        Debug.Log("Book button clicked.");
+    }
+
+     void ChequeFunction()
+    {
+        // Add code for the "Cheque" button functionality here.
+        Debug.Log("Cheque button clicked.");
+    }
+
+     void CutBookFunction()
+    {       
+            Book.SetActive(true);
+            gameObject.SetActive(false);
+    }
+
     public void GazeAt(bool gazing)
     {
         if (gazing)
